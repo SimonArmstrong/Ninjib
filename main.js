9087d7ddde2abb98b5b4c9fa563ba63c9b0ecfec
Simon Armstrong <strangemoosey@gmail.com>
2015-06-29 11:33:45 +1000

    this is the basic requirements for the code

---
+++ main.js
@@ -0,0 +1,64 @@
+var canvas = document.getElementById("gameCanvas");
+var context = canvas.getContext("2d");
+
+var startFrameMillis = Date.now();
+var endFrameMillis = Date.now();
+
+// This function will return the time in seconds since the function 
+// was last called
+// You should only call this function once per frame
+function getDeltaTime()
+{
+	endFrameMillis = startFrameMillis;
+	startFrameMillis = Date.now();
+
+		// Find the delta time (dt) - the change in time since the last drawFrame
+		// We need to modify the delta time to something we can use.
+		// We want 1 to represent 1 second, so if the delta is in milliseconds
+		// we divide it by 1000 (or multiply by 0.001). This will make our 
+		// animations appear at the right speed, though we may need to use
+		// some large values to get objects movement and rotation correct
+	var deltaTime = (startFrameMillis - endFrameMillis) * 0.001;
+	
+		// validate that the delta is within range
+	if(deltaTime > 1)
+		deltaTime = 1;
+		
+	return deltaTime;
+}
+
+//-------------------- Don't modify anything above here-----------------------------------------------------------------------------------------------
+
+function Run()
+{
+
+}
+
+
+//-------------------- Don't modify anything below here --------------------------------------------------------------------------------------------
+
+
+// This code will set up the framework so that the 'run' function is called 60 times per second.
+// We have a some options to fall back on in case the browser doesn't support our preferred method.
+(function() {
+  var onEachFrame;
+  if (window.requestAnimationFrame) {
+    onEachFrame = function(cb) {
+      var _cb = function() { cb(); window.requestAnimationFrame(_cb); }
+      _cb();
+    };
+  } else if (window.mozRequestAnimationFrame) {
+    onEachFrame = function(cb) {
+      var _cb = function() { cb(); window.mozRequestAnimationFrame(_cb); }
+      _cb();
+    };
+  } else {
+    onEachFrame = function(cb) {
+      setInterval(cb, 1000 / 60);
+    }
+  }
+  
+  window.onEachFrame = onEachFrame;
+})();
+
+window.onEachFrame(run);
