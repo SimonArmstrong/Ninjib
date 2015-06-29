fec07cc4dd1fa69c453ea1a7394e77656c733b3d
Simon Armstrong <strangemoosey@gmail.com>
2015-06-29 10:49:58 +1000

    Added basic Vector2 Class

---
+++ Vector2.js
@@ -0,0 +1,16 @@
+var Vector2 = function(x, y){
+	this.x = x;
+	this.y = y;
+}
+Vector2.prototype.add = function(vec){
+	return new Vector2(this.x + vec.x, this.y + vedc.y);
+}
+Vector2.prototype.subtract = function(vec){
+	return new Vector2(this.x - vec.x, this.y - vedc.y);
+}
+Vector2.prototype.multiply = function(vec){
+	return new Vector2(this.x * vec.x, this.y * vedc.y);
+}
+Vector2.prototype.divide = function(vec){
+	return new Vector2(this.x / vec.x, this.y / vedc.y);
+}
