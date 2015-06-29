9a8e5cd0bd0aa63fa6c6a5c9228be1bfeda8256f
Simon Armstrong <strangemoosey@gmail.com>
2015-06-29 11:56:12 +1000

    Pickups, there really isn't much for this yet.

---
+++ Pickups.js
@@ -0,0 +1,15 @@
+var coins = function 
+{
+	this.value = 10; //goes to score/money
+	this.sprite = new Sprite("");
+	
+	
+}
+
+var moneyBag = function 
+{
+	this.value = 100;
+	this.sprite = new Sprite ("");
+	
+	
+}
